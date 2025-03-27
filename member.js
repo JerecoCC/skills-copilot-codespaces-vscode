74d94c374034bb79b1bfef122f3676ec713d2e3a
Jereco Catingub <53340383+JerecoCC@users.noreply.github.com>
2025-03-27 18:14:54 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+  const skills = {
+    frontend: ['HTML', 'CSS', 'JS'],
+    backend: ['Node.js', 'Express.js'],
+    database: ['MongoDB', 'Firebase'],
+  };
+  return skills;
+}
